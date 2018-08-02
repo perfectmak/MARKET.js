e6ff4f3bfe20f280d6f82f771ff4c23c16609aac
Cryptomental <cryptomental.com@gmail.com>
2018-08-02 14:29:37 +0200

    test: Add Contract.test.ts and contract fills tests.
    Add tests to verify that getContractFillsAsync returns correct data.
    Correct OrderFilledEvent interface to satisfy lint.
    
    Refs: https://github.com/MARKETProtocol/MARKET.js/issues/95

--- src/types/OrderFilledEvent.ts
+++ src/types/OrderFilledEvent.ts
@@ -1,13 +1,13 @@
 import BigNumber from 'bignumber.js';
 
 export interface OrderFilledEvent {
-  maker: string;
-  taker: string;
-  feeRecipient: string;
-  filledQty: BigNumber;
-  paidMakerFee: BigNumber;
-  paidTakerFee: BigNumber;
-  price: BigNumber;
+  maker: string | BigNumber;
+  taker: string | BigNumber;
+  feeRecipient: string | BigNumber;
+  filledQty: number | BigNumber;
+  paidMakerFee: number | BigNumber;
+  paidTakerFee: number | BigNumber;
+  price: number | BigNumber;
   orderHash: string;
   txHash: string;
   blockNumber: number | null;
